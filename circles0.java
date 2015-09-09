504426d7343461abc32ebcc60536beb44a7f761c
clarc54 <chris20topher@yahoo.com>
2015-09-09 13:50:28 -0400

    Update circles0.java

--- circles0.java
+++ circles0.java
@@ -2,10 +2,10 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(400, 350);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 150,250, 150,175 );       // Try changing the (x,y) and size.
 }
